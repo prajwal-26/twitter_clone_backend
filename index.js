e1d6acb388b195b6b5c39499d7747d0e0503d5c8
prajwal <bablu2682002@gmail.com>
2023-11-05 14:52:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://admin:twitteradmin@cluster0.cbe1qop.mongodb.net/";
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    await client.connect();
+    const postCollection = client.db("database").collection("posts"); // this is post collection
+    const userCollection = client.db("database").collection("users"); // this is user collection
+
+    // get
+    app.get("/post", async (req, res) => {
+      const post = (await postCollection.find().toArray()).reverse();
+      res.send(post);
+    });
+    app.get("/user", async (req, res) => {
+      const user = await userCollection.find().toArray();
+      res.send(user);
+    });
+    app.get("/loggedInUser", async (req, res) => {
+      const email = req.query.email;
+      const user = await userCollection.find({ email: email }).toArray();
+      res.send(user);
+    });
+    app.get("/userPost", async (req, res) => {
+      const email = req.query.email;
+      const post = (
+        await postCollection.find({ email: email }).toArray()
+      ).reverse();
+      res.send(post);
+    });
+
+    // post
+    app.post("/post", async (req, res) => {
+      const post = req.body;
+      const result = await postCollection.insertOne(post);
+      res.send(result);
+    });
+    app.post("/register", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // patch
+    app.patch("/userUpdates/:email", async (req, res) => {
+      const filter = req.params;
+      const profile = req.body;
+      const options = { upsert: true };
+      const updateDoc = { $set: profile };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Twitter listening on port ${port}`);
+});
